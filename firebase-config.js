95760914340252b07bda68b912e21c70a9f17f46
POPASMALINOIS <j.m.p.garrido87@outlook.es>
2026-06-02 23:31:44 +0200

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+// firebase-config.js
+export const firebaseConfig = {
+  apiKey: "AIzaSyBJa8HqJmvldCCvYycbNx1OFbtVLTmZb20",
+  authDomain: "gesempa-control-horario.firebaseapp.com",
+  projectId: "gesempa-control-horario",
+  storageBucket: "gesempa-control-horario.firebasestorage.app",
+  messagingSenderId: "969679006732",
+  appId: "1:969679006732:web:1b367e3f31f0b7dc7f9229"
+};
+
+export const APP_COMPANY_ID = "gesempa-pymes";
+export const APP_COMPANY_NAME = "GESEMPA-PYMES";
