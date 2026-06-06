bad3b3bc25c7dd6ecec2cdde722f70531be941e3
POPASMALINOIS <j.m.p.garrido87@outlook.es>
2026-06-06 11:15:16 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2009,6 +2009,10 @@ if (els.exportExcelBtn) {
   els.exportExcelBtn.addEventListener("click", exportMonthlyExcel);
 }
 
+if (els.changePasswordBtn) {
+  els.changePasswordBtn.addEventListener("click", changeUserPassword);
+}
+
 if (els.recordModal) {
   els.recordModal.addEventListener("click", (event) => {
     if (event.target === els.recordModal) {
